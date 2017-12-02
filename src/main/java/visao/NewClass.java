0181a80db6a3ac2869107d1d127577ec7d886206
Murillo <murillo_eum@hotmail.com>
2017-12-02 20:06:43 -0300

    Atividade BD2

---
+++ src/main/java/visao/NewClass.java
@@ -0,0 +1,13 @@
+package visao;
+
+public class NewClass {
+    
+    public static void main(String[] args){
+        
+        Janela janela = new Janela();
+        janela.setVisible(true);
+        
+        
+    }
+    
+}
